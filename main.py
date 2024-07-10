abaec2bf1478afa04d5c7859179e40395c92155d
nhatphong5954 <pukinanasi27@gmail.com>
2024-07-10 20:55:08 +0700

    Chỉnh lại hàm trong file 525

--- main.py
+++ main.py
@@ -18,65 +18,13 @@ kernel = np.array((
  [0, 1, 0]), dtype="int")
 
 def main():
-    oper525.hit_or_miss_and_display(input_image, kernel)
+    oper525.hit_or_miss(input_image, kernel)
     #oper525.binary_region_filling(input_image, (1, 1))
     #oper525.grayscale_opening(input_image, kernel)
     #oper525.grayscale_morphology_gradient(input_image, kernel)
     #oper525.binary_opening(input_image, kernel)
     #oper525.binary_thinning(input_image)
     #oper525.grayscale_top_hat_transformation(input_image, kernel)
-
-    # # Ẩn cửa sổ do hàm Tk() gọi
-    # Tk().withdraw()
-
-    # # Người dùng chọn ảnh trên máy
-    # image_path = oper029.askopenfilename(title="Select a grayscale image",
-    #                             filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.bmp;*.tif;*.tiff")])
-
-    # # Kiểm tra file có tồn tại không
-    # if not image_path:
-    #     raise FileNotFoundError("No file selected. Please select an image file.")
-
-    # # Tải ảnh mức xám lên, nếu là ảnh màu thì không thông qua hàm này
-    # image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
-
-    # # Kiểm tra ảnh tải lên được không
-    # if image is None:
-    #     raise FileNotFoundError(f"The image could not be loaded. Please check the file and try again.")
-    
-    # choice = -1
-    # while choice != 0:
-
-    #     choice = oper029.menu()
-    #     if choice == "0":
-    #         print("Exit!")
-    #         exit()
-            
-        
-    #     if choice == "1":
-    #         ImageOutput = oper029.grayscale_smoothing(image)
-
-    #     elif choice == "2":
-    #         ImageOutput = oper029.grayscale_closing(image)
-            
-    #     elif choice == "3":
-    #         ImageOutput = oper029.binary_closing(image_path)
-        
-    #     elif choice == "4":
-    #         ImageOutput = oper029.grayscale_reconstruction(image)
-            
-    #     elif choice == "5":
-    #         ImageOutput = oper029.grayscale_textural_segmentation(image)
-
-    #     elif choice == "6":
-    #         ImageOutput = oper029.grayscale_granulometry(image)
-
-    #     elif choice == "7":
-    #         ImageOutput = oper029.binary_convex_hull(image_path)
-        
-    #     else:
-    #         print("Invalid choice. Please choose again.")
-    #         exit()
 
 if __name__ == "__main__":
     main()
